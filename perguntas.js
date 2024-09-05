4c498e4021b16dbddf5bb84be7bcb0c9bfeead13
kauavulgoimadexota13 <118731912+kauavulgoimadexota13@users.noreply.github.com>
2024-09-05 15:18:34 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,135 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual seria sua matéria favorita em Hogwarts?",
+        alternativas: [
+            {
+                texto: "Feitiços",
+                afirmacao: [
+                    "Seria da casa Corvinal. Os alunos dessa casa possuem características como inteligência, intelecto, valorizam o aprendizado e sabedoria. Corvinos tendem a ser estudantes proeminentes e bastante talentosos em meios acadêmicos."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Aula de voo",
+                afirmacao: [
+                    "Seria da casa Grifinoria. Os alunos dessa casa possuem características como coragem, cavalheirismo, ousadia e determinação. Eles geralmente são considerados nobres (de coração), embora as vezes tendem a ser imprudentes."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Qual desses locais te atrairia mais em Hogwarts?",
+        alternativas: [
+            {
+                texto: "Biblioteca de Hogwarts ",
+                afirmacao: [
+                    "Seria da casa Corvinal. Os alunos dessa casa possuem características como inteligência, intelecto, valorizam o aprendizado e sabedoria. Corvinos tendem a ser estudantes proeminentes e bastante talentosos em meios acadêmicos.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "A câmara secreta",
+                afirmacao: [
+                    "Seria da casa Grifinoria. Os alunos dessa casa possuem características como coragem, cavalheirismo, ousadia e determinação. Eles geralmente são considerados nobres (de coração), embora as vezes tendem a ser imprudentes."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você fosse viajar, para onde você iria?",
+        alternativas: [
+            {
+                texto: "Cruzeiro de luxo",
+                afirmacao: [
+                    "A casa seria Sonserina. Os alunos dessa casa possuem ambição, astúcia, inteligência e determinação. São também alunos com (um certo desprezo pelas regras), como em tempos disse Alvo Dumbledore."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Museus na Europa",
+                afirmacao: [
+                    "Seria da casa Grifinoria. Os alunos dessa casa possuem características como coragem, cavalheirismo, ousadia e determinação. Eles geralmente são considerados nobres (de coração), embora as vezes tendem a ser imprudentes."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O que você gosta de fazer no seu tempo livre?",
+        alternativas: [
+            {
+                texto: "Praticar esportes",
+                afirmacao: [
+                    "Seria da casa Lufa-Lufa. Os alunos dessa cas possuem características como dedicação, inteligência, pacientes e honestos. Suas admiráveis características os tornam trabalhadores árduos e modestos, sendo receptivos a todos os demais tipos de bruxos."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Ler um bom livro",
+                afirmacao: [
+                   "Seria da casa Corvinal. Os alunos dessa casa possuem características como inteligência, intelecto, valorizam o aprendizado e sabedoria. Corvinos tendem a ser estudantes proeminentes e bastante talentosos em meios acadêmicos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é sua estação do ano favorita?",
+        alternativas: [
+            {
+                texto: "Verão",
+                afirmacao: [
+                    "A casa seria Sonserina. Os alunos dessa casa possuem ambição, astúcia, inteligência e determinação. São também alunos com (um certo desprezo pelas regras), como em tempos disse Alvo Dumbledore."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Inverno",
+                afirmacao: [
+                    "Seria da casa Grifinoria. Os alunos dessa casa possuem características como coragem, cavalheirismo, ousadia e determinação. Eles geralmente são considerados nobres (de coração), embora as vezes tendem a ser imprudentes."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual dessas qualidades mais te atrai em uma pessoa?",
+        alternativas: [
+            {
+                texto: "Inteligência",
+                afirmacao: [
+                    "Seria da casa Corvinal. Os alunos dessa casa possuem características como inteligência, intelecto, valorizam o aprendizado e sabedoria. Corvinos tendem a ser estudantes proeminentes e bastante talentosos em meios acadêmicos."
+                ],
+            },
+            {
+                texto: "Elegância",
+                afirmacao: [
+                    "A casa seria Sonserina. Os alunos dessa casa possuem ambição, astúcia, inteligência e determinação. São também alunos com (um certo desprezo pelas regras), como em tempos disse Alvo Dumbledore."
+                ],
+                proxima: 9,
+            },
+        
+        ]
+    },
+    {
+        enunciado: "Você se considera uma pessoa introvertida ou extrovertida?",
+        alternativas: [
+            {
+                texto: "Introvertida",
+                afirmacao: [
+                    "Seria da casa Corvinal. Os alunos dessa casa possuem características como inteligência, intelecto, valorizam o aprendizado e sabedoria. Corvinos tendem a ser estudantes proeminentes e bastante talentosos em meios acadêmicos."
+                ],
+            },
+            {
+                texto: "Extrovertida",
+                afirmacao: [
+                    "Seria da casa Lufa-Lufa. Os alunos dessa cas possuem características como dedicação, inteligência, pacientes e honestos. Suas admiráveis características os tornam trabalhadores árduos e modestos, sendo receptivos a todos os demais tipos de bruxos."
+                ],
+                proxima: 10,
+            },
+        ]
+    }
+]
